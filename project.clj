5b95e3125cdf296d3d50e4eddb9369f543caddbb
Santiago Ciapina M. Salazar <santiago.ciapinams@gmail.com>
2026-05-19 17:05:51 -0300

    chore: add project dependencies

--- project.clj
+++ project.clj
@@ -1,10 +1,20 @@
 (defproject fret-calculator "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "https://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.12.2"]]
+  :description "API para cálculo de distâncias de trastes de violão "
+  :url "https://github.com/SantiagoCMS/fret-calculator.git"
+  :license {:name "MIT"
+            :url "https://opensource.org/licenses/MIT"}
+
+  :dependencies [[org.clojure/clojure "1.12.2"]
+                 [compojure "1.7.1"]
+                 [ring/ring-core "1.11.0"]
+                 [ring/ring-jetty-adapter "1.11.0"]
+                 [ring/ring-json "0.5.1"]
+                 [cheshire "5.12.0"]]
+
   :main ^:skip-aot fret-calculator.core
+
   :target-path "target/%s"
+
   :profiles {:uberjar {:aot :all
-                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
+                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}
+             :dev {:dependencies [[ring/ring-mock "0.4.0"]]}})
